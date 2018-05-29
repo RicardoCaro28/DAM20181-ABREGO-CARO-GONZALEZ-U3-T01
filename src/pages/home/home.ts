5909456a98daeea929377dcb9602972c9c001337
Alexia Ariana <alarabregode@ittepic.edu.mx>
2018-05-29 16:01:02 -0600

    import de dependencias

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, ToastController } from 'ionic-angular';
-
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { Contacts, Contact, ContactField, ContactName } from '@ionic-native/contacts';
 
 
 @Component({
