29c441341b56a45e8d9d5b95e4df2fa13f5619f5
Ricardo <luricarolo@ittepic.edu.mx>
2018-05-29 16:09:01 -0600

    Metodo para escanear

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -23,6 +23,44 @@ export class HomePage {
 
   }
 
+  scanear() {
+    this.barcodescanner.scan().then(data => {
+      this.escaneo = data.text;
+      this.cadena = data.text;
+
+      this.x = this.cadena.split(" ");
+      this.nombre = this.x[0];
+      this.apellido = this.x[1];
+      this.numero = this.x[2];
+
+      let contact: Contact = this.contacts.create();
+
+      contact.name = new ContactName(null, this.apellido, this.nombre);
+      contact.phoneNumbers = [new ContactField('mobile', this.numero)];
+      contact.save().then(
+        () => {
+          console.log('Contact saved!', contact);
+          let toast = this.toast.create({
+            message: 'Contacto añadido correctamente',
+            duration: 3000
+          });
+          toast.present();
+
+        },
+        (error: any) => {
+          console.error('Error saving contact.', error);
+          let toast = this.toast.create({
+            message: 'No se pudo agregar contacto',
+            duration: 3000
+          });
+          toast.present();
+
+        }
+      );
+
+    })
+
+  }
  
 
 
